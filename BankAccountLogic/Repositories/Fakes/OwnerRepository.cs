36b3442ef913de4966868886b1bf82d059bbeaa0
Christina <christina.golovach@gmail.com>
2018-11-12 19:48:17 +0300

    Add same changes in Owner Repository

--- BankAccountLogic/Repositories/Fakes/OwnerRepository.cs
+++ BankAccountLogic/Repositories/Fakes/OwnerRepository.cs
@@ -33,8 +33,13 @@ namespace BankAccountLogic.Repositories.Fakes
             {
                 throw new ArgumentNullException($"The {nameof(owner)} can not be null.");
             }
+            //TODO а еслі такого владельца нет, опять же это здесь чекать или можно просто remove?
+            Owner ownerForDelete = owners.FirstOrDefault(x => x.PassportNumber == owner.PassportNumber);
 
-            owners.Remove(owner);
+            if (!ReferenceEquals(ownerForDelete, null))
+            {
+                owners.Remove(owner);
+            }               
         }
 
         public void Update(Owner owner)
@@ -45,11 +50,13 @@ namespace BankAccountLogic.Repositories.Fakes
             }
 
             //TODO а если как раз-таки паспорт номер изменился???
-            Owner updatedOwner = owners.Find(x => x.PassportNumber == owner.PassportNumber);
-
-            updatedOwner.FirstName = owner.FirstName;
-            updatedOwner.LastName = owner.LastName;
-            updatedOwner.Email = owner.Email;
+            Owner ownerForUpdate = owners.FirstOrDefault(x => x.PassportNumber == owner.PassportNumber);
+            if (!ReferenceEquals(owner, null))
+            {
+                ownerForUpdate.FirstName = owner.FirstName;
+                ownerForUpdate.LastName = owner.LastName;
+                ownerForUpdate.Email = owner.Email;
+            }
         }
 
         public IEnumerable<Owner> GetAll()
